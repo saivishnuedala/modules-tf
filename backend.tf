5e3eb24c78c39af5b9e824714bd93929f2fb072e
Sai Vishnu Edala <saivishnu.edala@gmail.com>
2023-10-19 16:41:47 -0500

    dev: changes in network

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region  = "us-east-1"
     encrypt = true
-    bucket  = "tfbackendstore" #change bucket_name
+    bucket  = "tfbackendstore" #change bucket_name 
     key     = "dev/terraform.tfstate"
     dynamodb_table = "tfstatelock"
   }  
