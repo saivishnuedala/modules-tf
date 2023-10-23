fad858a6c747c0d020f432b6d18f6133cea0fb3d
Sai Vishnu Edala <saivishnu.edala@gmail.com>
2023-10-23 18:47:07 -0500

    dev: added storage module

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ module "my_server_module_2" {
   iam_instance_profile = module.ec2_role_module.instanceprofilename
 }
 
-# # S3 module
-# module "s3_module" {
-#   source = "./modules/storage"
-# }
+# S3 module
+module "s3_module" {
+  source = "./modules/storage"
+}
 
 # Flowlogs for vpc
 # module "flowlog_module" {
