8680c9962b7593a6018f5880e609c93ec83b9d85
Sai Vishnu Edala <saivishnu.edala@gmail.com>
2023-11-13 22:30:13 -0600

    added: backend statefile

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     region  = "us-east-1"
-#     encrypt = true
-#     bucket  = "bucket-name" #change bucket_name 
-#     key     = "terraform.tfstate"
-#     #dynamodb_table = "tfstatelock"
-#   }  
-# }
+terraform {
+  backend "s3" {
+    region  = "us-east-1"
+    encrypt = true
+    bucket  = "bucket-name" #change bucket_name 
+    key     = "terraform.tfstate"
+    #dynamodb_table = "tfstatelock"
+  }  
+}
